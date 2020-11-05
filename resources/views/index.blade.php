264347789659a91c847740fa8c278e2b90407a52
stuxnethdez <misaelhdez96@gmail.com>
2020-11-05 11:21:44 -0600

    mi primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Pagina Web - Misael</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+
+<body>
+    <div class="navbar navbar-light bg-light">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/ITTG_Escudo.png" width="52px" height="52px" alt="" >
+    </div>
+  
+<div class="jumbotron text-center">
+    <h1>Tecnológico de Tuxtla Gutiérrez<br></h1>
+    <p>Ingenieria en Sistemas Computacionales<br> Programación Web Con Frameworks-DS8<br>Misael Hernández Agustín</p> 
+</div>
+
+</body>
+</html>
